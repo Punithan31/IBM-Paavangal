8db8b0855f325d4ef53a55cd3be4eb8e4a306d50
Ramasubramaniam Gv <gvpranav747@gmail.com>
2022-11-14 11:34:30 +0530

    first commit

---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+   $name= $_POST["name"];
+   $email= $_POST["email"];
+    $Number = $_POST["Number"];
+    echo "Ny name is $name<br/> My mail id is $email<br/> My number is {$Number}"
+
+?>
